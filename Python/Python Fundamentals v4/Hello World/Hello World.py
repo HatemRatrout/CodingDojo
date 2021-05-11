12a75bae2592c638654a90fafde6a89cde3097b6
Hatem Ratrout <ratrouthatem@gmail.com>
2021-05-11 12:45:27 +0300

    update

---
+++ Python/Python Fundamentals v4/Hello World/Hello World.py
@@ -0,0 +1,16 @@
+print("Hello World") #Hello World
+
+Name = "Hatem" 
+print("Hello",Name) #Hello Hatem
+print("Hello"+Name) #HelloHatem
+
+Num = 10
+print("Hello",Num) #Hello 10
+print("Hello"+ Num) #Hello10
+
+#NINJA BONUS : store Num in string
+
+fave_food1 = "burger"
+fave_food2 = "pizza"
+print("I love to eat {} and {} ".format(fave_food1, fave_food2))  #I love to eat burger and pizza 
+print(f"I love to eat {fave_food1} and {fave_food2} ")  #I love to eat burger and pizza 
